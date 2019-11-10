b9d7eac8abd6e47835143426345c8f7ba315783a
wangshaoyong <418761605@qq.com>
2019-11-09 22:49:57 -0500

    update

---
+++ cmake-build-debug/CMakeFiles/CSA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSA.dir/main.cpp.o"
+  "CSA.pdb"
+  "CSA"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
